365c3d1f1c4a96a75e11b05c78a8374ba121c0ae
harshita9 <harshita.tupili@gmail.com>
2018-11-12 01:14:02 -0500

    Add files via upload

--- frontend.py
+++ frontend.py
@@ -4,10 +4,36 @@ from oauth2client.client import OAuth2WebServerFlow
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 import httplib2
+import redis
+import json
+import yaml
+import ast
 
 #-------------------- Beaker Stuff ---------------------------------------------
 import bottle
 from beaker.middleware import SessionMiddleware
+
+read_file = open('dump.json', 'r')
+data = json.load(read_file)
+read_file.close()
+
+page_ranks = data[0]['pageranks']
+inverted_index = data[0]['invertedIndex']
+document_index = data[0]['documentIndex']
+lexicons = data[0]['lexicon']
+
+inverted_index = inverted_index.replace("set(", "")
+inverted_index = inverted_index.replace(")", "")
+
+lexicons = yaml.load(lexicons)
+inverted_index = ast.literal_eval(inverted_index)
+document_index = ast.literal_eval(document_index)
+page_ranks = ast.literal_eval(page_ranks)
+
+#print(type(page_ranks))
+#print(type(document_index))
+#print(type(inverted_index))
+#print(inverted_index)
 
 session_opts = {
     'session.type': 'file',
@@ -20,7 +46,15 @@ sessionApp = SessionMiddleware(bottle.app(), session_opts)
 
 ##-------------GLOBAL VARIABLES FOR HOMEPAGE----------------------------------##
 history_counter = """ """
+word_counter = """ """
 history = dict()
+query = ""
+
+pageranked_urls = []
+
+PER_PAGE = 5
+
+num_pages = 0
 ## ---------------------------------------------------------------------------##
 
 #------------------- R O U T E S ----------------------------------------------#
@@ -29,7 +63,7 @@ history = dict()
 def homepage():
     output = 0
     session = request.environ.get('beaker.session')
-    print session
+    print (session)
 
     try: # Check logged in info
         email = session['email']
@@ -45,16 +79,49 @@ def homepage():
 def server_static(filename):
     return static_file(filename, root='./')
 
+@error(404)
+def error404(error):
+    output = template('error.tpl')
+    return output
+
 @route('/search', method='POST')
-def display_word_count():
+def search_keyword():
     # get input string/keywords
+    global query
     query = request.forms.get("keywords")
     query_lower = query.lower()
     # split the string and create a list of all words in string
     word_list = query_lower.split(" ")
     # dictionary to store word count
+
+    global lexicons
+    global inverted_index
+    global document_index
+    global page_ranks
+
+    # get search results
+    key_word = word_list[0]
+    urls = []
+    global pageranked_urls
+    pageranked_urls = []
+
+    key_word = "u'" + key_word + "'"
+
+    if key_word in lexicons:
+        key_word_value = lexicons[key_word]
+        url_id_list = inverted_index[key_word_value]
+        for url, url_id in document_index.items():
+            if url_id in url_id_list:
+                urls.append(url)
+        for pr, rank in page_ranks:
+            if pr in urls:
+                pageranked_urls.append(pr)
+
+    key_word = word_list[0]
+
     d = dict()
     global history
+    global word_counter
     # stores word count and word history in dictionaries
     for word in word_list:
         d[word] = d.get(word, 0) + 1
@@ -82,7 +149,7 @@ def display_word_count():
         history_counter += ("<tr><td>" + key + "</td><td>" + str(history[key]) + "</td></tr>")
         count += 1
 
-    session = request.environ.get('beaker.session')
+    """session = request.environ.get('beaker.session')
     output = 0;
     # Check logged in info
     try:
@@ -91,8 +158,48 @@ def display_word_count():
         email = session['email']
         output = template('table.tpl', keyword = query, table = word_counter, profilePicture = profilePicture, Name = userName, Email = email)
     except: # runs if no login info
-        output = template('tableAnon.tpl', keyword = query, table = word_counter)
+        output = template('tableAnon.tpl', keyword = query, table = word_counter, url_table = url_counter)
+    return output"""
+
+    rd = ("/search/%s/1" %(key_word))
+    redirect(rd)
+
+@route("/search//<page>")
+@route("/search/<keyword>/<page>")
+def pagination(keyword="nothing", page = '1'):
+
+    global history_counter
+    global word_counter
+    global pageranked_urls
+    global query
+
+    page = int(page)
+
+    global PER_PAGE
+    start = (page - 1) * PER_PAGE
+    end = page * PER_PAGE
+
+    parameters = {
+            'page': page,
+            'keyword': keyword,
+            'query': query,
+            'pageranked_urls': pageranked_urls,
+            'start': start,
+            'end': end,
+            }
+
+    session = request.environ.get('beaker.session')
+    output = 0;
+    # Check logged in info
+    try:
+        profilePicture = session['picture']
+        userName = session['name']
+        email = session['email']
+        output = template('pagination.tpl', key_word = query, table = word_counter, profilePicture = profilePicture, Name = userName, Email = email, **parameters)
+    except: # runs if no login info
+        output = template('paginationAnon.tpl', key_word = query, table = word_counter, **parameters)
     return output
+
 @route('/login')
 def logIn():
     flow = flow_from_clientsecrets("client_secret_346297252987-gessg0ftmins8qrsdkkh8lgv9ask1occ.apps.googleusercontent.com.json", scope='https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email', redirect_uri = "http://localhost:8080/redirect")
@@ -130,7 +237,7 @@ def logout():
 #Function saves the session information
 def saveSession(user_document):
         #save session
-        print user_document # checks the keys for user doc
+        print (user_document) # checks the keys for user doc
         session = request.environ.get('beaker.session')
         try:
             if user_document['name'] is '':
@@ -149,7 +256,7 @@ def saveSession(user_document):
             session['picture'] = "https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=350"
 
         session.save()
-        print "/redirect: session ", session
+        print ("/redirect: session ", session)
 
 
 
---
+++ pagination.tpl
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title> Results </title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="/static/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<div class="pull-right not_too_right">
+	<div class="dropdown">
+		<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+			<div class="profilePic">
+				<img src={{!profilePicture}} alt=" "></div>
+			<span class="caret"></span>
+		</button>
+		<ul class="dropdown-menu dropdown-menu-right">
+			<li><a href="#"> Hi {{!Name}}, </a></li>
+			<li><a href="#">{{!Email}}</a></li>
+			<li><a href="/logout"> Log Out</a></li>
+		</ul>
+	</div>
+</div>
+
+<body>
+	<div class="container ">
+
+		<div class="row h-100 justify-content-center align-items-center no-gutters">
+			<div class="engineLogo">
+				<img src="/static/beanlogo.png" style="width:17%;height:auto"; alt="Logo">
+			</div>
+		</div>
+
+		<div class="row h-100 justify-content-center align-items-center">
+			<class="col-6 justify-content-center">
+				<form action="/search" method="post">
+					<input class="form-control" name="keywords" placeholder="Search..." type="text" />
+				</form>
+				<div class="query-words">
+					<form action="/">
+						<input value="Back to Homepage" type="submit" class="btn btn-light" />
+					</form>
+				</div>
+		</div>
+		<div class="row h-100 justify-content-center align-items-center">
+			<div class="history_table ">
+				<h2> Search for "{{!keyword}}" </h2>
+			</div>
+		</div>
+	</div>
+
+	<%
+		page_num = 1
+		next_page = 1
+	%>
+
+	<%
+		if len(pageranked_urls) == 0:
+	%>
+			<h2> No results for "{{!key_word}}" </h2>
+			<h2> </h2>
+
+			% end
+
+	<%
+		for url in pageranked_urls[start : end]:
+	%>
+			<p style = "font-size:20px">
+				<a href="{{url}}">{{url}}</a>
+			</p>
+
+			% end
+
+	<%
+		 if page > 1:
+				page_num = page - 1
+	%>
+				<% ref = "/search/%s/%s" %(keyword, page_num) %>
+				<center style = "font-size:20px" ><a href="{{ref}}">Previous</a></center>
+		 % end
+
+	<%
+			if page < len(pageranked_urls)/5:
+				next_page = page + 1
+	%>
+				<% ref = "/search/%s/%s" %(keyword, next_page) %>
+				<center style = "font-size:20px"><a href="{{ref}}">Next</a></center>
+			% end
+
+</body>
+
+</html>
